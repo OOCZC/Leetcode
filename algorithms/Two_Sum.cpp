525f626ccedb3a5db4f8fd9b2a2fb4d49ef34342
OOC <whatisooc@gmail.com>
2018-01-15 14:15:55 +0800

    Update Two_Sum.cpp

--- algorithms/Two_Sum.cpp
+++ algorithms/Two_Sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*  O(n*n)解法
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
         for(int i = 0; i < nums.size(); ++i){
@@ -9,6 +10,23 @@ public:
                     ans.push_back(j);
                     return ans;
                 }
+            }
+        }
+    }
+*/ 
+// O(n)解法
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); ++i){
+	    map[nums[i]] = i;
+	}
+        for(int i = 0; i < nums.size(); ++i){
+	    int temp = target - nums[i];
+            if(map.count(temp) && i != map[temp]){
+                ans.push_back(i);
+                ans.push_back(map[temp]);
+                return ans;
             }
         }
     }
